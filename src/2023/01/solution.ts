eed8e104f2b32016e43c586227a0ed8a8d9b61ba
Maciej Szostak <maciek.szostak55@gmail.com>
2023-12-01 21:49:01 +0100

    Part One solution

--- src/2023/01/solution.ts
+++ src/2023/01/solution.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+import fs from "fs";
+import path from "path";
+
+const filePath = path.resolve(__dirname, "input.txt");
+const inputData = fs.readFileSync(filePath, "utf-8").trim().split("\n");
+
+function partOne(input: string[]) {
+  const values = input.map((line) => {
+    let firstValue = line
+      .split("")
+      .find((value) => !Number.isNaN(Number(value)));
+    let lastValue = line
+      .split("")
+      .findLast((value) => !Number.isNaN(Number(value)));
+
+    return Number(firstValue + lastValue);
+  });
+
+  return values.reduce((sum, value) => sum + value);
+}
+
+console.log(partOne(inputData));
