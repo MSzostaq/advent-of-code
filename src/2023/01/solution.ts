df8b2e8df0bca830754350f95907c285303160ac
Maciej Szostak <maciek.szostak55@gmail.com>
2023-12-01 22:21:28 +0100

    Part Two solution

--- src/2023/01/solution.ts
+++ src/2023/01/solution.ts
@@ -4,7 +4,7 @@ import path from "path";
 const filePath = path.resolve(__dirname, "input.txt");
 const inputData = fs.readFileSync(filePath, "utf-8").trim().split("\n");
 
-function partOne(input: string[]) {
+function partOne(input: string[]): number {
   const values = input.map((line) => {
     let firstValue = line
       .split("")
@@ -19,4 +19,38 @@ function partOne(input: string[]) {
   return values.reduce((sum, value) => sum + value);
 }
 
+export function partTwo(input: string[]): number {
+  return input
+    .map((line) => {
+      const digits = line
+        .split("")
+        .map((char, index) => {
+          const wordDigitMap: Record<string, number> = {
+            one: 1,
+            two: 2,
+            three: 3,
+            four: 4,
+            five: 5,
+            six: 6,
+            seven: 7,
+            eight: 8,
+            nine: 9,
+          };
+
+          for (const word in wordDigitMap) {
+            if (line.slice(index).startsWith(word)) {
+              return wordDigitMap[word];
+            }
+          }
+
+          return parseInt(char) || 0;
+        })
+        .filter((number) => number !== 0);
+
+      return parseInt("" + digits[0] + digits[digits.length - 1]) || 0;
+    })
+    .reduce((a, b) => a + b, 0);
+}
+
 console.log(partOne(inputData));
+console.log(partTwo(inputData));
