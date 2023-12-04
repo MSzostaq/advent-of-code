80c6b57309a54521ca8fd32747ef2384c87fc558
Maciej Szostak <maciek.szostak55@gmail.com>
2023-12-04 13:37:05 +0100

    Part Two solution

--- src/2023/03/solution.ts
+++ src/2023/03/solution.ts
@@ -57,4 +57,65 @@ const partOne = (input: string[][]) => {
   return sum;
 };
 
+const partTwo = (input: string[][]) => {
+  let gearNumbers: { [key: string]: number[] } = {};
+
+  for (let y = 0; y < input.length; y++) {
+    let currentNumber = "",
+      checkNumber = false,
+      gearLocation = null;
+
+    for (let x = 0; x < input[y].length; x++) {
+      if (input[y][x].match(/[0-9]/) && !checkNumber) {
+        checkNumber = true;
+        currentNumber = "";
+        gearLocation = null;
+      }
+
+      if (
+        (x == input[y].length - 1 || !input[y][x].match(/[0-9]/)) &&
+        checkNumber
+      ) {
+        if (gearLocation)
+          gearNumbers[gearLocation].push(
+            parseInt(
+              currentNumber + (input[y][x].match(/[0-9]/) ? input[y][x] : "")
+            )
+          );
+        checkNumber = false;
+      }
+
+      if (checkNumber) {
+        currentNumber += input[y][x];
+
+        for (let j = -1; j <= 1; j++) {
+          for (let i = -1; i <= 1; i++) {
+            if (i == 0 && j == 0) continue;
+            if (
+              y + j < 0 ||
+              y + j >= input.length ||
+              x + i < 0 ||
+              x + i >= input[y].length
+            )
+              continue;
+
+            const char = input[y + j][x + i];
+            if (char == "*") {
+              gearLocation = `${x + i},${y + j}`;
+              if (gearNumbers[gearLocation] == null)
+                gearNumbers[gearLocation] = [];
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return Object.values(gearNumbers).reduce((sum, array) => {
+    if (array.length == 2) sum += array[0] * array[1];
+    return sum;
+  }, 0);
+};
+
 console.log(partOne(inputData));
+console.log(partTwo(inputData));
