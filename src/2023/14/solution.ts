c9411d4cff9e9636ea869e13dd9dd54b39ca6994
Maciej Szostak <maciek.szostak55@gmail.com>
2023-12-14 09:41:42 +0100

    Part One solution

---
+++ src/2023/14/solution.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import path from "path";
+
+const filePath = path.resolve(__dirname, "input.txt");
+const inputData = fs.readFileSync(filePath, "utf-8");
+
+const partOne = (input: string) => {
+  let match;
+  const cols: string[] = [];
+  const rows = input.split("\n");
+  for (let i = 0; i < rows[0].length; i++)
+    cols.push(rows.map((r) => r[i]).join(""));
+
+  return cols.reduce<number>((sum, col) => {
+    const rockRx = /[O.]+/g; // ew i know
+    while ((match = rockRx.exec(col))) {
+      if (!match[0].includes("O")) continue;
+      for (let i = 0; i < match[0].split("O").length - 1; i++) {
+        sum += cols.length - match.index - i;
+      }
+    }
+    return sum;
+  }, 0);
+};
+
+console.log(partOne(inputData));
