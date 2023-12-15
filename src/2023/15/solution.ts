80b6c2dd3aab0352b2e0bd479f938dcf5f8f1516
Maciej Szostak <maciek.szostak55@gmail.com>
2023-12-15 17:25:42 +0100

    Part Two solution

--- src/2023/15/solution.ts
+++ src/2023/15/solution.ts
@@ -15,4 +15,25 @@ const partOne = (input: string) => {
   return sequence.map(hash).reduce((sum, v) => sum + v, 0);
 };
 
+const partTwo = (input: string) => {
+  const boxes = [...Array(256)].map(() => new Map<string, number>());
+
+  input.split(",").forEach((str) => {
+    if (str.at(-1) === "-") {
+      const label = str.slice(0, -1);
+      boxes[hash(label)].delete(label);
+    } else {
+      const [label, n] = str.split("=");
+      boxes[hash(label)].set(label, Number(n));
+    }
+  });
+
+  return boxes
+    .flatMap((box, bi) =>
+      [...box.values()].map((fl, fli) => (bi + 1) * (fli + 1) * fl)
+    )
+    .reduce((sum, v) => sum + v, 0);
+};
+
 console.log(partOne(inputData));
+console.log(partTwo(inputData));
