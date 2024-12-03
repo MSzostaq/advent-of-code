467838e32ca400aaa6bcb64d1c2fc6e601bc8814
Maciej Szostak <maciek.szostak55@gmail.com>
2024-12-03 15:40:15 +0100

    Part two solution

--- src/2024/03/solution.ts
+++ src/2024/03/solution.ts
@@ -7,6 +7,10 @@ const filePath = path.resolve(
 );
 const inputData = fs.readFileSync(filePath, "utf-8").trim();
 
+function parseInput(rawInput: string) {
+  return rawInput.split("\n");
+}
+
 function partOne(input: string | string[]) {
   const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
 
@@ -22,4 +26,27 @@ function partOne(input: string | string[]) {
   return sum;
 }
 
+function partTwo(input: string | string[]) {
+  const regex = /do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)/g;
+
+  let enabled = true;
+  let sum = 0;
+  let match;
+
+  const inputString = Array.isArray(input) ? input.join("\n") : input;
+
+  while ((match = regex.exec(inputString)) !== null) {
+    if (match[0] === "do()") {
+      enabled = true;
+    } else if (match[0] === "don't()") {
+      enabled = false;
+    } else if (enabled) {
+      sum += parseInt(match[1]) * parseInt(match[2]);
+    }
+  }
+
+  return sum;
+}
+
 console.log("Part 1:", partOne(inputData));
+console.log("Part 2:", partTwo(inputData));
