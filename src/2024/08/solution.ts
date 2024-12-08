8c9a7156573cdf943f6885ab1fe4b89ef51f9559
Maciej Szostak <maciek.szostak55@gmail.com>
2024-12-08 15:38:10 +0100

    Part two solution

--- src/2024/08/solution.ts
+++ src/2024/08/solution.ts
@@ -64,8 +64,82 @@ function partOne(input: string[]): number {
   const map = parseMap(input);
   const locations = findLocations(map);
   const antinodeCount = calculateAntinodes(map, locations);
-
   return antinodeCount;
 }
 
+function partTwo(input: string[]): number {
+  const map = parseMap(input);
+  const locations = findLocations(map);
+  const width = map[0].length;
+  const height = map.length;
+  const antinodes: [number, number][] = [];
+
+  Object.values(locations).forEach((locs) => {
+    if (locs.length <= 1) return;
+
+    for (let i = 0; i < locs.length; i++) {
+      for (let j = 0; j < locs.length; j++) {
+        if (i === j) continue;
+
+        const [xi, yi] = locs[i];
+        const [xj, yj] = locs[j];
+
+        const antinode1 = [xi, yi];
+        while (
+          antinode1[0] >= 0 &&
+          antinode1[1] >= 0 &&
+          antinode1[0] < width &&
+          antinode1[1] < height
+        ) {
+          antinode1[0] += xi - xj;
+          antinode1[1] += yi - yj;
+          if (
+            !antinodes.some(
+              ([ax, ay]) => ax === antinode1[0] && ay === antinode1[1]
+            ) &&
+            antinode1[0] >= 0 &&
+            antinode1[1] >= 0 &&
+            antinode1[0] < width &&
+            antinode1[1] < height
+          ) {
+            antinodes.push([antinode1[0], antinode1[1]]);
+          }
+        }
+
+        const antinode2 = [xj, yj];
+        while (
+          antinode2[0] >= 0 &&
+          antinode2[1] >= 0 &&
+          antinode2[0] < width &&
+          antinode2[1] < height
+        ) {
+          antinode2[0] += xj - xi;
+          antinode2[1] += yj - yi;
+          if (
+            !antinodes.some(
+              ([ax, ay]) => ax === antinode2[0] && ay === antinode2[1]
+            ) &&
+            antinode2[0] >= 0 &&
+            antinode2[1] >= 0 &&
+            antinode2[0] < width &&
+            antinode2[1] < height
+          ) {
+            antinodes.push([antinode2[0], antinode2[1]]);
+          }
+        }
+
+        if (!antinodes.some(([ax, ay]) => ax === xi && ay === yi)) {
+          antinodes.push([xi, yi]);
+        }
+        if (!antinodes.some(([ax, ay]) => ax === xj && ay === yj)) {
+          antinodes.push([xj, yj]);
+        }
+      }
+    }
+  });
+
+  return antinodes.length;
+}
+
 console.log("Part 1:", partOne(inputData));
+console.log("Part 2:", partTwo(inputData));
