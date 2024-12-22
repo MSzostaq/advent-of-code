9a8a86d9880e8ec48d8b4fcccbda6e80a84ba9d5
Maciej Szostak <maciek.szostak55@gmail.com>
2024-12-22 10:45:02 +0100

    Part two solution

--- src/2024/22/solution.ts
+++ src/2024/22/solution.ts
@@ -25,4 +25,38 @@ function partOne(input: string) {
     }, 0n);
 }
 
+function partTwo(input: string) {
+  const ranges: { [key: string]: number[] } = {};
+  input
+    .split("\n")
+    .map((num) => BigInt(num))
+    .forEach((num) => {
+      let seed = num;
+      const visited = new Set<string>();
+      const changes: number[] = [];
+      for (let i = 0; i < 2000; i++) {
+        const nextSeed = evolveSecretNumber(seed);
+        changes.push(Number((nextSeed % 10n) - (seed % 10n)));
+        seed = nextSeed;
+
+        if (changes.length === 4) {
+          const key = changes.join(",");
+          if (!visited.has(key)) {
+            if (ranges[key] === undefined) ranges[key] = [];
+            ranges[key].push(Number(nextSeed % 10n));
+            visited.add(key);
+          }
+          changes.shift();
+        }
+      }
+    });
+
+  return Math.max(
+    ...Object.values(ranges).map((range) =>
+      range.reduce((sum, num) => sum + num, 0)
+    )
+  );
+}
+
 console.log("Part 1:", partOne(inputData));
+console.log("Part 2:", partTwo(inputData));
