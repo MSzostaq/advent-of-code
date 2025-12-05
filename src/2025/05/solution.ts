4c6d88b7b69a04b1f72a1f880b80063303e1f777
Maciej Szostak <maciek.szostak55@gmail.com>
2025-12-05 15:03:34 +0100

    Part two solution

--- src/2025/05/solution.ts
+++ src/2025/05/solution.ts
@@ -19,7 +19,6 @@ function partOne(lines: string[]): number {
   }
 
   let freshCount = 0;
-
   for (const id of ingredientIds) {
     const isFresh = ranges.some(([start, end]) => id >= start && id <= end);
     if (isFresh) {
@@ -30,4 +29,39 @@ function partOne(lines: string[]): number {
   return freshCount;
 }
 
+function partTwo(lines: string[]): number {
+  const blankLineIndex = lines.findIndex((line) => line === "");
+  const ranges: [number, number][] = [];
+
+  for (let i = 0; i < blankLineIndex; i++) {
+    const [start, end] = lines[i].split("-").map(Number);
+    ranges.push([start, end]);
+  }
+
+  ranges.sort((a, b) => a[0] - b[0]);
+  const merged: [number, number][] = [];
+
+  for (const [start, end] of ranges) {
+    if (merged.length === 0) {
+      merged.push([start, end]);
+    } else {
+      const [lastStart, lastEnd] = merged[merged.length - 1];
+
+      if (start <= lastEnd + 1) {
+        merged[merged.length - 1] = [lastStart, Math.max(lastEnd, end)];
+      } else {
+        merged.push([start, end]);
+      }
+    }
+  }
+
+  let totalFresh = 0;
+  for (const [start, end] of merged) {
+    totalFresh += end - start + 1;
+  }
+
+  return totalFresh;
+}
+
 console.log("Part 1:", partOne(input));
+console.log("Part 2:", partTwo(input));
